f8bc3c27503c1c214def0078fd0a19250494c2dc
Octyl312 <126682164+Octyl312@users.noreply.github.com>
2023-10-01 21:02:40 -0500

    Update Database.js

--- Database.js
+++ Database.js
@@ -3,6 +3,10 @@ import { createClient } from '@supabase/supabase-js'
 // Create a single supabase client for interacting with your database
 const supabase = createClient('https://rjhwvtbdfxvprvcpmqvm.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqaHd2dGJkZnh2cHJ2Y3BtcXZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ3MTQ3NDMsImV4cCI6MjAxMDI5MDc0M30.p2LS3j7jcTVwXxWg0O1HkwF_97wK8foSzW7DNjDTfq4')
 
+// Author of this code: Adam Reese
+// Date last modified: 10/1/2023
+// Current WIP for connecting to supabase
+
 // WIP. this variables are to be changed when wanting to add or remove something from database. currently working on being able to user input variables.
 var playerNo = null;
 var name = null;
