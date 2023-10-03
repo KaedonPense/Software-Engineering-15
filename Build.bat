267fdf5b0fb66b6877b6f23aa7c33f50e5809e15
KaedonPense <143656224+KaedonPense@users.noreply.github.com>
2023-10-03 15:39:23 -0500

    Update Build.bat - added udp stuff

--- Build.bat
+++ Build.bat
@@ -1,8 +1,8 @@
 ::@echo off
-javac Main.java PlayerEntryScreen.java StartScreen.java
+javac Main.java PlayerEntryScreen.java StartScreen.java udpBrodcast.java udpClient.java
 if %errorlevel% neq 0 (
 	echo There was an error; exiting now.	
 ) else (
-	echo Compiled correctly!  Running Game...
+	echo Compiled correctly!  Running Program...
 	java Main
 )
