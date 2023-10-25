22e20d26490aa8b4de7ffe819e65b51d63e041f7
Octyl312 <126682164+Octyl312@users.noreply.github.com>
2023-10-25 14:07:14 -0500

    Update Build.bat

--- Build.bat
+++ Build.bat
@@ -1,5 +1,5 @@
 ::@echo off
-javac Main.java PlayerEntryScreen.java StartScreen.java udpBroadcast.java udpClient.java
+javac Main.java PlayerEntryScreen.java StartScreen.java udpBroadcast.java udpClient.java Database.java
 if %errorlevel% neq 0 (
 	echo There was an error; exiting now.	
 ) else (
